7c00009dd0400063bc7deacc5c3bde438d0dc4d0
Archit Jain <0xarchit@gmail.com>
2025-03-13 18:15:13 +0530

    implemented loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
+import { Atom } from 'react-loading-indicators';
 import { ApiKeyInput } from './components/ApiKeyInput';
 import { Chat } from './components/Chat';
 import { useStore } from './store/useStore';
 
 function App() {
-  const { apiKey, userName } = useStore(state => ({
+  const { apiKey, userName, isLoading } = useStore(state => ({
     apiKey: state.apiKey,
     userName: state.userName,
+    isLoading: state.isLoading,
   }));
 
   return (
     <div className="min-h-screen bg-gray-100">
-      {!apiKey || !userName ? <ApiKeyInput /> : <Chat />}
+      {isLoading ? (
+          <div className="fixed inset-0 flex items-center justify-center w-full h-full  bg-black">
+            <Atom color="#ffffff" size="large" text="Loading..." textColor="#ffffff" />
+          </div>
+      ) : !apiKey || !userName ? (
+        <ApiKeyInput />
+      ) : (
+        <Chat />
+      )}
     </div>
   );
 }
